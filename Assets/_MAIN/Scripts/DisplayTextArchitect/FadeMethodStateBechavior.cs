344f26900013f6f88a31e0ed0cda04704f1ba8a7
ganariya <ganariya2525@gmail.com>
2023-02-24 11:53:26 +0900

    :sparkles: fade building

--- Assets/_MAIN/Scripts/DisplayTextArchitect/FadeMethodStateBechavior.cs
+++ Assets/_MAIN/Scripts/DisplayTextArchitect/FadeMethodStateBechavior.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 public class FadeMethodStateBehavior : IBuildMethodStateBehavior
 {
+    private const byte ALPHA_THRESHOLD = 20;
+    private const float FADE_SPEED_MULTIPLIER = 2f;
+
     private readonly DisplayTextArchitect arch;
 
     public FadeMethodStateBehavior(DisplayTextArchitect displayTextArchitect)
@@ -12,7 +15,38 @@ public class FadeMethodStateBehavior : IBuildMethodStateBehavior
 
     public IEnumerator Building()
     {
-        yield return null;
+        int left = arch.PrevText.Length;
+        int right = left + 1;
+        var textInfo = arch.TmProText.textInfo;
+        var alphas = new float[textInfo.characterCount];
+
+        // called per frame
+        while (true)
+        {
+            float fadeSpeed = (arch.HurryUp ? arch.AppearCharactersNumPerFrame * 5 : arch.AppearCharactersNumPerFrame) * arch.BaseSpeed * FADE_SPEED_MULTIPLIER;
+
+            for (int i = left; i < right; i++)
+            {
+                var cInfo = textInfo.characterInfo[i];
+                if (!cInfo.isVisible) continue;
+
+                var vColors = textInfo.meshInfo[cInfo.materialReferenceIndex].colors32;
+                alphas[i] = Mathf.MoveTowards(alphas[i], 255, fadeSpeed);
+
+                for (int v = 0; v < 4; v++) vColors[cInfo.vertexIndex + v].a = (byte)alphas[i];
+                if (alphas[i] >= 255) left++;
+            }
+            arch.TmProText.UpdateVertexData(TMPro.TMP_VertexDataUpdateFlags.Colors32);
+
+            bool lastVisible = textInfo.characterInfo[right - 1].isVisible;
+            if (!lastVisible || alphas[right - 1] >= ALPHA_THRESHOLD)
+            {
+                if (right < textInfo.characterCount) right++;
+                else if (!lastVisible || alphas[right - 1] >= 255) break;
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
     }
 
     public void Prepare()
@@ -33,8 +67,8 @@ public class FadeMethodStateBehavior : IBuildMethodStateBehavior
         {
             var cInfo = textInfo.characterInfo[i];
             if (!cInfo.isVisible) continue;
-            var vertexColors = textInfo.meshInfo[cInfo.materialReferenceIndex].colors32;
-            for (int v = 0; v < 4; v++) vertexColors[cInfo.vertexIndex + v] = new Color(arch.TextColor.r, arch.TextColor.g, arch.TextColor.b, i < arch.PrevText.Length ? 1 : 0);
+            var vColors = textInfo.meshInfo[cInfo.materialReferenceIndex].colors32;
+            for (int v = 0; v < 4; v++) vColors[cInfo.vertexIndex + v] = new Color(arch.TextColor.r, arch.TextColor.g, arch.TextColor.b, i < arch.PrevText.Length ? 1 : 0);
         }
         arch.TmProText.UpdateVertexData(TMPro.TMP_VertexDataUpdateFlags.Colors32);
     }
