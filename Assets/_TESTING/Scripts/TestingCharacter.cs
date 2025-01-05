faf91fcab51d4922f26d5c183c6a5160fcca5d94
ganyariya <ganariya2525@gmail.com>
2025-01-05 10:15:44 +0900

    implement chap5-2

--- Assets/_TESTING/Scripts/TestingCharacter.cs
+++ Assets/_TESTING/Scripts/TestingCharacter.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Core.Characters;
+using Core.DisplayDialogue;
 using UnityEngine;
 
 namespace Testing
@@ -9,8 +10,32 @@ namespace Testing
     {
         void Start()
         {
+            // Character ganyariya = CharacterManager.instance.CreateCharacter("ganyariya");
+            // Character notFound = CharacterManager.instance.CreateCharacter("notFound");
+
+            StartCoroutine(CharacterSpeakTest());
+        }
+
+        IEnumerator CharacterSpeakTest()
+        {
             Character ganyariya = CharacterManager.instance.CreateCharacter("ganyariya");
-            Character notFound = CharacterManager.instance.CreateCharacter("notFound");
+            Character you = CharacterManager.instance.CreateCharacter("you");
+
+            var dialogues = new List<string> {
+                "\"こんにちは！{a}私は ganyariya です。\"",
+                // `""` で囲まないと会話と認識されないことに注意する
+                "\"あなたの名前をおしえて\"",
+            };
+
+            yield return ganyariya.Say(dialogues);
+
+            var yourDialogues = new List<string> {
+                "\"私の名前は you です。\"",
+                "\"この世界における架空的存在です\"",
+            };
+            yield return you.Say(yourDialogues);
+
+            Debug.Log("CharacterSpeakTest: End");
         }
     }
 }
