adbef05dc9c836852014897044e67802d6e8151b
ganyariya <ganariya2525@gmail.com>
2025-01-26 13:21:36 +0900

    10-2: livd2d Motion をプレイできるようにする

--- Assets/_MAIN/Scripts/Core/Characters/CharacterTypes/Live2DCharacter.cs
+++ Assets/_MAIN/Scripts/Core/Characters/CharacterTypes/Live2DCharacter.cs
@@ -6,8 +6,20 @@ namespace Core.Characters
 {
     public class Live2DCharacter : Character
     {
+        /// <summary>
+        /// BaseCharacter に Animator が存在する
+        /// 一方 Live2D にはデフォルトで設定されている motionAnimator がありそちらを変わりに利用する
+        /// </summary>
+        private Animator live2DMotionAnimator;
+
         public Live2DCharacter(string name, CharacterConfig config, GameObject prefab, string rootCharacterFolder) : base(name, config, prefab, rootCharacterFolder)
         {
+            live2DMotionAnimator = animator.transform.GetChild(0).GetComponentInChildren<Animator>();
+        }
+
+        public void playMotion(string animationName)
+        {
+            live2DMotionAnimator.Play(animationName);
         }
     }
 }
